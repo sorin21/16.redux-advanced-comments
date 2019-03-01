3eca6d0d1bf6eaa32b9420a28482fc8911b812b0
Sorin <sorin21us@yahoo.com>
2019-03-02 00:28:21 +0200

    Adding redux thunk

--- src/actions/resultActions.js
+++ src/actions/resultActions.js
@@ -1,9 +1,9 @@
 export const STORE_RESULT = 'STORE_RESULT';
-export const storeResult = (counter) => {
-  return {
+export const storeResult = (counter) => dispatch => {
+  dispatch({
     type: STORE_RESULT,
     result: counter
-  }
+  })
 }
 
 export const DELETE_RESULT = 'DELETE_RESULT';
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-
+import thunk from 'redux-thunk';
 import reducer from './reducer';
 import './index.css';
 import App from './App';
@@ -21,7 +21,7 @@ const logger = (store) => next => action => {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ latency: 0 }) || compose;
 const store = createStore(reducer, composeEnhancers(
-  applyMiddleware(logger)
+  applyMiddleware(logger, thunk)
 ));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
